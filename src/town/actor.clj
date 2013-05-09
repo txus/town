446c4307629013ff11c4fdfe1cc8faef2c0f1df0
Josep M. Bach <josep.m.bach@gmail.com>
2013-05-09 14:08:09 +0200

    Document actor

--- src/town/actor.clj
+++ src/town/actor.clj
@@ -2,15 +2,30 @@
 
 (def actors (atom []))
 
-(defn make! [state handler]
-  (let [actor (agent {:state state :handler handler})]
+(defn make!
+  "Creates a new actor with an initial state and a handler function.
+  The handler function takes an actor and a message and must return an actor,
+  either the same or a new one. For example:
+
+    (make!
+      {:foo :bar} ; initial state
+      (fn [actor message]
+        (println message)
+        (assoc actor :state {:baz :quux}))) ; we return an extended actor
+
+  The handler function may have side effects."
+  [initial-state handler]
+  (let [actor (agent {:state initial-state :handler handler})]
     (swap! actors conj actor)
     actor))
 
-(defn send! [receiver message]
-  (let [handler (:handler @receiver)]
-    (send-off receiver handler message)
+(defn send!
+  "Asynchronously sends a message to a specific actor."
+  [actor message]
+  (let [handler (:handler @actor)]
+    (send-off actor handler message)
     nil))
 
 (defn spread! [message]
+  "Asynchronously sends a message to a random actor."
   (send! (rand-nth @actors) message))
